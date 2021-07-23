d8140035b90c6edcc0ecfc9f42a0ba61b4775a05
Karen Baney <Karen_Baney@SHAMROCKFOODS.com>
2021-07-23 08:23:05 -0700

    Final page layout solution

---
+++ src/components/header.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <router-link to="/">Home</router-link>
+      </li>
+      <li>
+        <router-link to="/about">About</router-link>
+      </li>
+      <li>
+        <router-link to="/second-layout">Shout Out</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+<style scoped>
+ul, li {
+  list-style: none;
+}
+</style>
---
+++ src/layouts/second.vue
@@ -0,0 +1,6 @@
+<template>
+  <div id="app">
+    <router-view name="header" />
+    <router-view name="content" />
+  </div>
+</template>
